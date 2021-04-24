b8d67a9b2ad352166cc202c8a09a9169baf382fe
kgentel <katie.m.gentel@gmail.com>
2021-04-24 17:53:25 -0500

    added current date to header

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// add current date
+var todayMoment = moment();
+$('#currentDay').text(todayMoment.format('dddd, MMM Do'));
+
+// 3 columns using bootstrap
+
+// moment time to left column
+
+
+// ability to add text to middle column
+
+
+// add save button with local storage and save icon
